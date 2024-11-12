8acd4a467eb1e1a209eb85a0e2f3ba0bf772de0e
Andrii Kudenko <andrii.kudenko@gmail.com>
2024-11-12 18:59:45 -0500

    Make migrations to database

--- alembic/env.py
+++ alembic/env.py
@@ -1,34 +1,45 @@
 import asyncio
 from logging.config import fileConfig
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
-from sqlalchemy import pool
 from alembic import context
-from community_telegram_bot.database.models import Base, User  # Make sure to import your models
-
-# Load the Alembic configuration
+from community_telegram_bot.database.database import Base
+from community_telegram_bot.database.models import *
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 
 # Interpret the config file for Python logging.
-fileConfig(config.config_file_name)
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
-# Add your models' MetaData object here
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
 DATABASE_URL = config.get_main_option("sqlalchemy.url")
-engine = create_async_engine(DATABASE_URL, poolclass=pool.NullPool)
 
-def run_migrations():
-    """Run migrations in 'online' mode."""
-    connectable = engine.connect()
-    with connectable as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True
-        )
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
 
-        with context.begin_transaction():
-            context.run_migrations()
+connectable = create_async_engine(DATABASE_URL)
+
+async def run_migrations_online():
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+asyncio.run(run_migrations_online())
 
 if __name__ == "__main__":
-    asyncio.run(run_migrations())
+    asyncio.run(run_migrations_online())
